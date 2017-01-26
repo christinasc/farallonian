3be1598f225bdb568d74351d10875f81603b0b94
Christina <christinasc@gmail.com>
2017-01-26 15:32:44 -0800

    new repo

---
+++ application.py
@@ -0,0 +1,73 @@
+import logging
+import logging.handlers
+import waterApplication
+import os
+
+
+from wsgiref.simple_server import make_server
+
+
+# Create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Handler 
+#LOG_FILE = '/opt/python/log/sample-app.log'
+LOG_FILE = '/tmp/sample-app.log'
+handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1048576, backupCount=5)
+handler.setLevel(logging.INFO)
+
+# Formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Add Formatter to Handler
+handler.setFormatter(formatter)
+
+# add Handler to Logger
+logger.addHandler(handler)
+
+welcome =""
+indexFile = "index.html"
+
+def readHtmlFile(myfile):
+  with open(myfile) as mf:
+      fileContent = mf.read()
+  return fileContent
+
+
+def application(environ, start_response):
+    path    = environ['PATH_INFO']
+    method  = environ['REQUEST_METHOD']
+    if method == 'POST':
+        try:
+            if path == '/':
+                request_body_size = int(environ['CONTENT_LENGTH'])
+                request_body = environ['wsgi.input'].read(request_body_size).decode()
+                logger.info("Received message: %s" % request_body)
+            elif path == '/scheduled':
+                logger.info("Received task %s scheduled at %s", environ['HTTP_X_AWS_SQSD_TASKNAME'], environ['HTTP_X_AWS_SQSD_SCHEDULED_AT'])
+        except (TypeError, ValueError):
+            logger.warning('Error retrieving request body for async work.')
+        response = ''
+    else:
+        response = welcome
+        
+    status = '200 OK'
+    headers = [('Content-type', 'text/html')]
+
+    start_response(status, headers)
+    return [response]
+
+
+if __name__ == '__main__':
+
+#    waterApplication.readConfigFile()
+#    response , browser =  waterApplication.handleWaterLogin()
+#    acctInfo =  waterApplication.getWaterAccountMain(response)
+
+    welcome = readHtmlFile(indexFile)
+    # welcome = readHtmlFile("wat.html")
+    
+    httpd = make_server('', 8000, application)
+    print("Serving on port 8000...")
+    httpd.serve_forever()
---
+++ waterApplication.py
@@ -0,0 +1,96 @@
+import mechanicalsoup, re
+from sys import argv
+import os
+
+
+
+loginInfo = { 'LOGIN':"login", 
+              'PASSWORD':"password",
+              'USER_NAME': "username"}
+
+configFile = "login.config.me"
+URL = "https://www.midpeninsulawater.org/billpay/"
+
+def readConfigFile():
+    with open(configFile) as fp:
+        for line in fp:
+            entry = line.split(":")
+            key = entry[0].strip()
+            val = entry[1].strip()
+            loginInfo[key] = val 
+ #   print("this is logininfo", loginInfo)
+    
+def writeFile(content, filename):
+    with open(filename, 'w') as op:
+        op.write(content)
+        op.close()
+
+
+def getLoginInfo(browser):
+    login_page = browser.get(URL)
+    login_form = login_page.soup.find("form", {"class":"ywploginform"})
+
+    login_form.find("input", {"name": "login[username]"})["value"]= loginInfo['LOGIN']
+    login_form.find("input", {"name": "login[password]"})["value"] = loginInfo['PASSWORD']
+    response = browser.submit(login_form, login_page.url)
+    return response
+
+def handleWaterLogin():
+    # create a browser object
+    browser = mechanicalsoup.Browser()
+    response = getLoginInfo(browser)
+    if response:
+        print("Your're connected as " + loginInfo['USER_NAME'])
+       # print response
+    else:
+        print("Not connected")
+    return response, browser
+
+
+def getWaterAccountMain(response):
+    acctText = ""
+    acctInfo = response.soup
+    if acctInfo:
+#        acctText = acctInfo.get_text() # get text only
+        acctText = acctInfo
+#    print ("Got acct info")
+   # print(acctInfo)
+    writeFile(str(acctInfo), "acctInfo.html")
+    return acctText
+ 
+
+def getWaterBillHistory(response, browser):
+    historyPage = ""
+    for link in response.soup.find_all('a'):
+        availUrls = str(link.get('href'))
+        if re.search(r"history", availUrls):
+            print "link found for history, following...."
+            print(link.get('href'))
+            history_page = browser.get(availUrls)
+            if history_page:
+                    print ("Got history page")
+                    historyPage =  history_page.soup.get_text()
+                    #print(historyPage)
+                    writeFile(str(history_page.soup), "waterHistory.html")
+    return historyPage
+
+
+
+def waterProcess():
+    readConfigFile()
+    response , browser =  handleWaterLogin()
+    acctInfo =  getWaterAccountMain(response)
+    historyPage = getWaterBillHistory(response, browser)
+
+
+def main():
+    waterProcess()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+#  print " ================================"
